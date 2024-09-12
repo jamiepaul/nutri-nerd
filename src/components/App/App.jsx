b243af48bd8b16c0d440e75a45e9fab4ce048405
Jamie Paul <jbirthler@gmail.com>
2024-09-12 14:21:47 -0400

    initial project files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+import Header from '../Header/Header';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.card}>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import reactLogo from '../../assets/react.svg';
+import viteLogo from '/vite.svg';
+import styles from './Header.module.css';
+
+function Header() {
+	return (
+		<header>
+			<div className={styles['logo-row']}>
+				<a href="https://vitejs.dev" target="_blank">
+					<img
+						src={viteLogo}
+						className={styles.logo}
+						alt="Vite logo"
+					/>
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img
+						src={reactLogo}
+						className={`${styles.logo} ${styles.react}`}
+						alt="React logo"
+					/>
+				</a>
+			</div>
+			<h1>Nutri&#8226;Nerd</h1>
+		</header>
+	);
+}
+
+export default Header;

b2b15b73725bd58a55e2ef020e0e7d86c30a171b
Jamie Paul <jbirthler@gmail.com>
2024-09-13 17:13:39 -0400

    add helper for averages and display in TrackedAverages

--- src/components/TrackedAverages/TrackedAverages.jsx
+++ src/components/TrackedAverages/TrackedAverages.jsx
@@ -1,28 +1,41 @@
+import { useContext, useMemo } from 'react';
+import { MacrosContext } from '../MacrosProvider/MacrosProvider';
+import { average } from '../../helpers/math.helpers';
+
 import styles from './TrackedAverages.module.css';
-
-// function getAverage() {
-
-// }
 
 function TrackedAverages() {
 	console.log('TrackedAverages rendered');
+	const { dailyMacros } = useContext(MacrosContext);
+
+	const avg = useMemo(() => {
+		const avgProtein = average(dailyMacros.map(entry => entry.protein));
+		const avgCarbs = average(dailyMacros.map(entry => entry.carbs));
+		const avgFat = average(dailyMacros.map(entry => entry.fat));
+
+		return {
+			protein: avgProtein,
+			carbs: avgCarbs,
+			fat: avgFat
+		};
+	}, [dailyMacros]);
 
 	return (
 		<div className={styles.average}>
-			<h2>Averages</h2>
+			<h2>Tracked Averages</h2>
 			<p>From X to X (todo add dates)</p>
 			<dl className={styles.values}>
 				<div className={styles.value}>
 					<dt>Protein</dt>
-					<dd>130</dd>
+					<dd>{Math.round(avg.protein)}</dd>
 				</div>
 				<div className={styles.value}>
 					<dt>Carbs</dt>
-					<dd>200</dd>
+					<dd>{Math.round(avg.carbs)}</dd>
 				</div>
 				<div className={styles.value}>
 					<dt>Fat</dt>
-					<dd>55</dd>
+					<dd>{Math.round(avg.fat)}</dd>
 				</div>
 			</dl>
 		</div>
--- src/helpers/math.helpers.js
+++ src/helpers/math.helpers.js
@@ -2,3 +2,9 @@
 export const random = (min, max) => {
 	return Math.floor(Math.random() * (max - min)) + min;
 };
+
+// Calculate the average of an array of values
+export const average = (array) => {
+	const totalSum = array.reduce((sum, currentValue) => sum + currentValue, 0);
+	return totalSum / array.length;
+}
